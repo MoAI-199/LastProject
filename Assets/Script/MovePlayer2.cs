37baea0a02061a039a8905134b3f771d1a44f0e5
Kousuke1121 <66938559+Kousuke1121@users.noreply.github.com>
2022-09-05 15:15:07 +0900

    1004_藤代プレイヤー2作成

---
+++ Assets/Script/MovePlayer2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer2 : MonoBehaviour
+{
+    private Pearent _pearent;
+
+    void Start()
+    {
+        _pearent = GetComponent<Pearent>();
+    }
+
+    void Update()
+    {
+        Vector2 position = transform.position;
+        float speed = _pearent.getSpeed();
+        if (Input.GetKey("left"))
+        {
+            position.x -= speed;
+        }
+        if (Input.GetKey("right"))
+        {
+            position.x += speed;
+        }
+        if (Input.GetKey("up"))
+        {
+            position.y += speed;
+        }
+        if (Input.GetKey("down"))
+        {
+            position.y -= speed;
+        }
+
+        transform.position = position;
+    }
+}
