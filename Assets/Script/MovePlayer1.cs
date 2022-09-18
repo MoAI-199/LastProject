f759f0f74cae3f660967830629fa24c476ef6cbd
tca_hatanaka <moairyunosuke@gmail.com>
2022-09-18 10:56:35 +0900

    1015_畑中_移動フラグの追加

--- Assets/Script/MovePlayer1.cs
+++ Assets/Script/MovePlayer1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlayer1 : MonoBehaviour {
+public class MovePlayer1 : MoveCommonBase {
 
     private Pearent _pearent;
     void Start( ) {
@@ -10,19 +10,28 @@ public class MovePlayer1 : MonoBehaviour {
     }
 
     void Update( ) {
+        move( );
+    }
+
+    private void move( ){
         Vector2 position = transform.position;
         float speed = _pearent.getSpeed( );
+        setMoving( false );
         if( Input.GetKey( KeyCode.A ) ) {
             position.x -= speed;
-        } 
+            setMoving( true );
+        }
         if( Input.GetKey( KeyCode.D ) ) {
             position.x += speed;
+            setMoving( true );
         }
         if( Input.GetKey( KeyCode.W ) ) {
             position.y += speed;
+            setMoving( true );
         }
         if( Input.GetKey( KeyCode.S ) ) {
             position.y -= speed;
+            setMoving( true );
         }
 
         transform.position = position;
