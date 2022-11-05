6ebed2d62e3e69df1940f80291fd4e7d7974e256
tca_hatanaka <moairyunosuke@gmail.com>
2022-11-05 18:04:03 +0900

    Defineの実装

--- Assets/Script/System/GameManager.cs
+++ Assets/Script/System/GameManager.cs
@@ -8,12 +8,10 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager instatnce;
 
-    private COMMON_DATA.GAME_MODE _game_mode = COMMON_DATA.GAME_MODE.NONE;
+    private COMMON_DATA.GAME_MODE _game_mode = COMMON_DATA.GAME_MODE.CHELLENGE;
     
     private COMMON_DATA.GAME_STATE_TYPE _game_state = COMMON_DATA.GAME_STATE_TYPE.GAME_PLAYING;
-    
-    
-    
+
     private void Awake( ) {
         if( instatnce == null ) {
             instatnce = this;
@@ -23,10 +21,9 @@ public class GameManager : MonoBehaviour {
         }
     }
     private void Start( ) {
-        
+        settingDefine( );
     }
     private void Update( ) {
-        
     }
 
     public COMMON_DATA.GAME_MODE getGameMode( ){
@@ -41,5 +38,22 @@ public class GameManager : MonoBehaviour {
     public void setGameMode( COMMON_DATA.GAME_MODE game_mode ) {
         _game_mode = game_mode;
     }
- 
+
+    public void settingDefine(){
+        //ゲームステータスの設定
+#if GAME_PLAYING
+        _game_state = COMMON_DATA.GAME_STATE_TYPE.GAME_PLAYING;
+#elif GAME_SETTING
+        _game_state = COMMON_DATA.GAME_STATE_TYPE.GAME_SETTING;
+#endif
+
+        //ゲームモードの設定
+#if CHELLENGE
+        _game_mode =  COMMON_DATA.GAME_MODE.CHELLENGE;
+#elif PVP
+        _game_mode =  COMMON_DATA.GAME_MODE.PVP;
+#endif
+
+    }
+
 }
